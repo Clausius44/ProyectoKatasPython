5509f66f5792731eccca4ee92a0a7c0084deb975
ECL_Clausius44 <eduard.colo.llom@gmail.com>
2024-12-16 09:12:55 +0100

    Update Ej04.py

--- Ejercicios/Ej04.py
+++ Ejercicios/Ej04.py
@@ -2,18 +2,13 @@
 4. Genera una función que calcule la diferencia entre los valores de dos listas. Usa la función map()
 """
 
-def diferinator(list1, list2):
+def diference(num1, num2):
 
-    if len(list1) > len(list2): print("La primera lista contiene mas elementos que la segunda")
-    if len(list1) < len(list2): print("La segunda lista contiene mas elementos que la primera")
-
-    answerList = []
-    for num1, num2 in zip(list1, list2):
-        answerList.append(num1 - num2)
-
-    return answerList
+    return num1 - num2
 
 testLista1 = [2, 4, 5, 2, 7, 9, 12]
 testLista2 = [3, 5, 7, 2, 1, 8]
 
-print(diferinator(testLista2, testLista1))
+difList = list(map(diference, testLista1, testLista2))
+
+print(difList)
